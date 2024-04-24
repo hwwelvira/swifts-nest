9d0662f2030ab621186251ac6c063a3d5c1f58bd
Nagi-ovo <101612750+Nagi-ovo@users.noreply.github.com>
2024-04-25 01:30:29 +0800

    feat: waline comment added

---
+++ src/components/Waline.tsx
@@ -0,0 +1,29 @@
+import {
+  init,
+  type WalineInitOptions,
+  type WalineInstance,
+} from '@waline/client';
+import React, { useEffect, useRef } from 'react';
+import '@waline/client/style';
+
+export type WalineOptions = Omit<WalineInitOptions, 'el'> & { path: string };
+
+export const Waline = (props: WalineOptions) => {
+  const walineInstanceRef = useRef<WalineInstance | null>(null);
+  const containerRef = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    walineInstanceRef.current = init({
+      ...props,
+      el: containerRef.current,
+    });
+
+    return () => walineInstanceRef.current?.destroy();
+  }, [containerRef, props]);
+
+  useEffect(() => {
+    walineInstanceRef.current?.update(props);
+  }, [props]);
+
+  return <div ref={containerRef} />;
+};
